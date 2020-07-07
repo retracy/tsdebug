66b8299eb0183e1248b2f5933a64047671f1180f
Bob Tracy <retracy@live.com>
2020-07-07 15:50:05 -0700

    Investigating IChoices

--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import { of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
- 
-const switched = of(1, 2, 3).pipe(switchMap((x: number) => of(x, x * 2, x * 3)));
-switched.subscribe(x => console.log(x));
