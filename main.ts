8716a04df09ba3bae0bc28b3545f083b40f5757d
Bob Tracy <retracy@live.com>
2022-11-15 11:14:03 -0800

    updated

--- main.ts
+++ main.ts
@@ -1,19 +1,13 @@
-import { Observable, of } from 'rxjs';
-import { tap, take } from 'rxjs/operators';
+let data = [
+  {
+    name: "a",
+    val: 1,
+  },
+  {
+    name: "b",
+    val: 2,
+  },
+];
 
-interface IChoices {
-    [id: string] : string | {
-        choices?: IChoices
-    }
-}
-
-class Choices implements IChoices {
-    [id: string]: string | { choices?: IChoices; };
-}
-
-let c: IChoices = {
-    "en": "Hello",
-    "fr": "no idea"
-}
-
-console.log(c)
+let x = data.find(n => n.name.includes('a')).val
+console.log(x);
